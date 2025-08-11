e841b4bd80c4801caa5ceb4834e19e9244f2d673
actlceo <actl.ceo@gmail.com>
2025-08-11 23:05:57 +0530

    3 files to be committed to main

--- app.js
+++ app.js
@@ -6,3 +6,18 @@ function greetUser(name) {
 }
 
 console.log(greetUser("Developer"));
+// Add to app.js (at the bottom)
+
+// New feature: Calculator functions
+function add(a, b) {
+    return a + b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+// Test the calculator
+console.log("Calculator test:");
+console.log(`5 + 3 = ${add(5, 3)}`);
+console.log(`4 * 6 = ${multiply(4, 6)}`);
